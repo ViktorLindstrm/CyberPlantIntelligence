bd7211f007f616dbc3be4c51cbbffcf5f1034e76
ViktorLindstrm <viktor.lindstrm@gmail.com>
2017-02-19 11:14:17 +0100

    updated arduino nodemcu code

--- apps/nodemcu/sensor.ino
+++ apps/nodemcu/sensor.ino
@@ -2,9 +2,11 @@
 #include <ESP8266HTTPClient.h>
 
 const int AnalogIn  = A0;
-const char *SERVER_WIFI_SSID = "****";
-const char *SERVER_WIFI_PASS = "****";
-const long sleepTimeS = 30*60;
+const char *SERVER_WIFI_SSID = "*******";
+const char *SERVER_WIFI_PASS = "*******";
+const long sleepTimeS = 30 * 60;
+const String name = "real2";
+
 void setupWiFi()
 {
    Serial.print("Connecting to WiFi ");
@@ -23,31 +25,27 @@ void stopWiFi(){
 }
 void setup() {
    
-  
+  pinMode(2, OUTPUT);
+  digitalWrite(2, LOW);
+  delay(500);
   int readingIn = analogRead(AnalogIn);
-  
+  int retVal = ((readingIn * 100) / 1024);
   setupWiFi();
 
   HTTPClient http;
-  http.begin("http://192.168.1.135:8080/sensor/real2");
+  http.begin("http://192.168.1.135:8080/sensor/"+name);
   http.addHeader("Content-Type", "application/json");
 
   
   String pld = "{\"data\":\"";
-  pld +=readingIn;
+  pld +=retVal;
   pld +=+"\"}";
   
   http.POST(pld);
-  
-  
   http.end();
  
   ESP.deepSleep(sleepTimeS * 1000000);
-
 }
 
 void loop() {
-
- 
-
 }
