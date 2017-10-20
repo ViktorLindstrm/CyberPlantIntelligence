708f1c2b414cf35633cdbc3105bf1fd967f73eba
ViktorLindstrm <viktor.lindstrm@gmail.com>
2017-10-20 23:20:14 +0200

    Trying to find issue

--- apps/rest_hello_world/src/leds_handler.erl
+++ apps/rest_hello_world/src/leds_handler.erl
@@ -2,7 +2,7 @@
 -export([init/2]).
 -export([content_types_provided/2]).
 -export([content_types_accepted/2]).
--export([send_update/2,get_update/2]).
+-export([send_update/2]).
 -export([allowed_methods/2]).
 
 init(Req, Opts) ->
@@ -13,7 +13,7 @@ allowed_methods(Req, State) ->
 
 content_types_accepted(Req, State) ->
     {[
-      {<<"application/json">>, get_update}
+      %{<<"application/json">>, get_update}
      ],
      Req, State}.
 
@@ -22,23 +22,22 @@ content_types_provided(Req, State) ->
       {<<"application/json">>, send_update}
      ], Req, State}.
 
-get_update(Req,State) ->
-    {ok, Data, Req2} = cowboy_req:read_body(Req, #{length => 1000000, period => 5000}),
-    io:format("Data: ~p~n",[Data]),
-    PumpIdBin = cowboy_req:binding(id, Req2),
-    PumpId = erlang:binary_to_atom(PumpIdBin,utf8),
-    case plantsys_mng:get_pump(PumpId) of
-        {error,_} ->  %%If no node exists
-            plantsys_mng:add_pump(PumpId),
-            io:format("PumpId: ~p~nStatus: ~p~n",[PumpId,Data]);
-        _ -> undefined
-    end,
-    {true,Req2,State}.
+%get_update(Req,State) ->
+    %{ok, Data, Req2} = cowboy_req:read_body(Req, #{length => 1000000, period => 5000}),
+    %io:format("Data: ~p~n",[Data]),
+    %PumpIdBin = cowboy_req:binding(id, Req2),
+    %PumpId = erlang:binary_to_atom(PumpIdBin,utf8),
+    %case plantsys_mng:get_pump(PumpId) of
+        %{error,_} ->  %%If no node exists
+            %plantsys_mng:add_pump(PumpId),
+            %io:format("PumpId: ~p~nStatus: ~p~n",[PumpId,Data]);
+        %_ -> undefined
+    %end,
+    %{true,Req2,State}.
 
 send_update(Req,State) ->
     LedsIdBin = cowboy_req:binding(id, Req),
     LedsId = erlang:binary_to_atom(LedsIdBin,utf8),
-    io:format("Got hi from ~p~n",[LedsId]),
     case plantsys_mng:get_led(LedsId) of
         {error,not_found} ->  %%If no node exists
             %io:format("Error: ~p~n",[E]),
@@ -54,4 +53,5 @@ send_update(Req,State) ->
                   end,
     %LedsRGB = maps:get(color,Status),
     JsonStatus = jiffy:encode(Status),
+    io:format("Got hi from ~p~nanswering with: ~p~n",[LedsId,JsonStatus]),
     {JsonStatus, Req, State}.
